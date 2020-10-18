e845f082e4208da051e489e4c3b6b6f880a0ef56
Hernani Mattos <hernani.luiz@gmail.com>
2020-10-17 21:40:20 -0300

    adjusts

---
+++ src/views/vivaReal/containers/vivaReal/product/ProductDetails.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="product-details">
+		<Card
+			v-if="productDetails['images']"
+			:image="productDetails.images"
+		/>
+	</div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+const { mapActions, mapState } = createNamespacedHelpers('grupoZap');
+import Card from '@/components/ui/Card';
+export default {
+	name: 'ProductsDetails',
+	computed: {
+		...mapState(['productDetails'])
+	},
+	methods: {
+		...mapActions(['getProductById'])
+	},
+	created() {
+		const { id } = this.$route.params;
+		if (id) {
+			this.getProductById(id);
+		}
+	},
+	components: {
+		Card
+	}
+};
+</script>
